f6bdd2c62eaa4d8a14a134290fe33ae29dfa43db
ibrahim abdellatif <55769032+ibrahimabdellatif@users.noreply.github.com>
2020-11-26 16:55:14 +0200

    binary search

---
+++ src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // user input
+        System.out.println("please int number of element ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("enter element of " + (i + 1));
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Please Enter The Number That You Searched For ");
+        int key = sc.nextInt();
+        //first index in array is always 0
+        binarySearch(arr , 0 ,n-1 , key);
+//    for manual input
+//        int [] arr = new int[] {10,20,30,40,50};
+//        int key = 40;
+//        binarySearch(arr , 0 , arr.length-1 , key);
+    }
+    public static void binarySearch(int []arr , int first , int last , int key){
+        int mid = (first + last)/2;
+        while (first<last){
+            if (arr[mid]<key){
+                first = mid+1;
+            }else if (arr[mid] == key){
+                System.out.println("the element is found");
+                break;
+            }else {
+                last = mid-1;
+            }
+        }if (first>last){
+            System.out.println("Element is not Found !!");
+        }
+    }
+}
